321b11e01608b5f9f6412ca3cbf57c80712e7b3b
abdulmajid <abdulmajid.malikov3@gmail.com>
2024-08-14 11:09:28 +0500

    first commit

---
+++ src/test/java/com/malikov/paymeclone/PaymeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malikov.paymeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymeCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
